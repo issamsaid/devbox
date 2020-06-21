3caa56be9a84b9afeef6f7253a1e81afe5914ad7
Issam SAID <said.issam@gmail.com>
2020-06-22 00:31:14 +0400

    add trancparency and add default config file

--- files/i3/bin/daemon.d/htop.sh
+++ files/i3/bin/daemon.d/htop.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #echo "* example.sh"
-#echo 'Put your shell to ${color fa3}~/.i3/bin/daemon.d${color} in order to '
+#echo 'Put your shell to ${color fa3}~/.config/i3/bin/daemon.d${color} in order to '
 #echo 'show the output here'
 #htop
--- files/i3/bin/i3bar.sh
+++ files/i3/bin/i3bar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-i3status -c ~/.i3/i3status.conf | while :
+i3status -c ~/.config/i3/i3status.conf | while :
 do
     read line
-    network=$(~/.i3/bin/network )
+    network=$(~/.config/i3/bin/network )
     echo "$network | $line" || exit 1
 done
