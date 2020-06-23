ff80dabcd2098b2c43c75d621845acd8f887b030
Issam SAID <isaid@nvidia.com>
2020-06-23 23:47:24 +0400

    clean scripts

--- scripts/ubuntu-pre-reqs.sh
+++ scripts/ubuntu-pre-reqs.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # install ubuntu pre-reqs:
 sudo apt -y update && sudo apt -y upgrade
-sudo apt remove lightdm gdm3
-sudo apt-get purge compiz-gnome
-sudo apt-get autoremove -y --purge compiz-gnome
+sudo apt -y remove lightdm gdm3
+sudo apt -y purge compiz-gnome
+sudo apt -y autoremove 
 
-sudo apt install -y git python python3 curl i3 i3blocks python-pip python3-pip 
-sudo apt remove --purge ansible 
+sudo apt -y install git python python3 curl i3 i3blocks python-pip python3-pip 
+sudo apt -y remove --purge ansible 
 sudo -H pip uninstall -q ansible > /dev/null
 sudo -H pip3 uninstall -q ansible > /dev/null
-sudo apt install -y ansible
+sudo apt -y install ansible
 
-sudo apt remove --purge libreoffice* firefox
-sudo apt remove --purge xdg-user-dirs thunderbird
+sudo apt -y remove --purge libreoffice* firefox
+sudo apt -y remove --purge xdg-user-dirs thunderbird
 
 sudo add-apt-repository ppa:kgilmer/speed-ricer
-sudo apt-get -y update
-sudo apt-get install -y i3-gaps
+sudo apt -y update
+sudo apt -y install i3-gaps
 
