2d4a578ddab6943d4f4adfa01abac874667a0d62
Issam SAID <isaid@nvidia.com>
2020-07-27 09:33:43 +0000

    add command line options to choose the displays

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,16 +1,84 @@
-#!/bin/bash 
-# get active monitors:
-n=$(xrandr --listactivemonitors | head -n 1 | cut -d' ' -f2)
-echo "${n} monitor(s) detected"
-monitors=$(xrandr --listactivemonitors | tail -n ${n}  | cut -d' ' -f 6)
-if [ ${n} -ge 1 ] ; then
-	primary=$(echo ${monitors} | cut -d' ' -f1)
+#!/bin/bash
+function usage () {
+cat <<EOF
+...
+... usage: ${0##*/} [options]
+... --
+... options:
+...   -h|--help        show this message
+... -f=*|--factor=*    zoom factor
+... -p=*|--primary=*   primary display
+... -s=*|--secondary=* secondary display
+...
+EOF
+exit 0
+}
+
+# function to parse command line:
+function parse_cmdline () {
+  for i in "$@"; do
+    case $i in
+  	  -h|--help)
+        usage; break
+  	  ;;
+   	  -f=*|--factor=*)
+        ZFACTOR="${i#*=}"
+  	  	shift
+  	  ;;
+      -p=*|--primary=*)
+        primary="${i#*=}"
+  	  	shift
+  	  ;;
+      -s=*|--secondary=*)
+        secondary="${i#*=}"
+  	  	shift
+  	  ;;
+      *)
+        usage
+        shift
+      ;;
+    esac
+  done
+}
+# default values:
+ZFACTOR="1.0"
+xft_base=96
+primary=""
+secondary=""
+# parse cmd line:
+parse_cmdline "$@"
+# try to detect the displays:
+if xrandr --listactivemonitors >/dev/null 2>&1 ; then
+  n=$(xrandr --listactivemonitors | head -n 1 | cut -d' ' -f2)
+  echo "${n} monitor(s) detected"
+  monitors=$(xrandr --listactivemonitors | tail -n ${n}  | cut -d' ' -f 6)
+  if [ ${n} -ge 1 ] ; then
+    primary=$(echo ${monitors} | cut -d' ' -f1)
+  fi
+  if [ ${n} -ge 2 ] ; then
+    secondary=$(echo ${monitors} | cut -d' ' -f2)
+  else
+    secondary=${primary}
+  fi
 fi
-if [ ${n} -ge 2 ] ; then
-	secondary=$(echo ${monitors} | cut -d' ' -f2)
+# make sure we set the right monitors:
+if [ -z "${primary}" ] ; then
+  primary="primary"
+  secondary="primary"
 else
-	secondary=${primary}
+  if [ -z "${secondary}" ] ; then
+    secondary="${primary}"
+  fi
 fi
+# calculate xft:
+#XFT=$(( rint( $(echo "${xft_base}*${ZFACTOR}" | bc) ) ))
+XFT=$(echo "${xft_base}*${ZFACTOR}" | bc)
+XFT=$(echo "${XFT}/1" | bc)
+# show info about displays:
 echo "- primary: ${primary}"
 echo "- secondary: ${secondary}"
-ansible-playbook -k -K -i inventory setup.yml --extra-vars "username=${USER} userhome=${HOME} hostname="${HOSTNAME}" primary=${primary} secondary=${secondary}"
+echo "- xft: ${XFT}"
+# run the ansible playbook:
+ansible-playbook -k -K   \
+  -i inventory setup.yml \
+  --extra-vars "xft=${XFT} username=${USER} userhome=${HOME} hostname="${HOSTNAME}" primary=${primary} secondary=${secondary}"
--- scripts/ubuntu-pre-reqs.sh
+++ scripts/ubuntu-pre-reqs.sh
@@ -3,17 +3,17 @@
 sudo apt -y update && sudo apt -y upgrade
 sudo apt -y remove lightdm gdm3
 sudo apt -y purge compiz-gnome
-sudo apt -y autoremove 
+sudo apt -y autoremove
 
-sudo apt -y install git python python3 curl python-pip python3-pip 
-sudo apt -y install git i3 i3lock i3blocks
-sudo apt -y remove --purge ansible 
+sudo apt -y install git python python3 curl python-pip python3-pip
+sudo apt -y install x11-xserver-utils xinit i3 i3lock i3blocks
+sudo apt -y remove --purge ansible
 sudo -H pip uninstall -q ansible > /dev/null
 sudo -H pip3 uninstall -q ansible > /dev/null
 sudo apt -y install ansible
 
 sudo apt -y remove --purge libreoffice* firefox
-sudo apt -y remove --purge xdg-user-dirs thunderbird
+sudo apt -y remove --purge thunderbird
 
 sudo add-apt-repository ppa:kgilmer/speed-ricer
 sudo apt -y update
