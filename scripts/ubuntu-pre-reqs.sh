bab8350972f5c24f3ee9ee88ab78d64134f3da48
Issam SAID <isaid@nvidia.com>
2020-08-27 17:26:24 +0400

    update pre-req scripts

--- scripts/ubuntu-pre-reqs.sh
+++ scripts/ubuntu-pre-reqs.sh
@@ -7,11 +7,12 @@ sudo apt -y autoremove
 
 sudo apt -y install git python python3 curl python-pip python3-pip
 sudo apt -y install x11-xserver-utils xinit i3 i3lock i3blocks
-sudo apt -y remove --purge ansible
-sudo -H pip uninstall -q ansible > /dev/null
-sudo -H pip3 uninstall -q ansible > /dev/null
-sudo apt -y install ansible
-
+#sudo apt -y remove --purge ansible
+#sudo -H pip uninstall -q ansible > /dev/null
+#sudo -H pip3 uninstall -q ansible > /dev/null
+if ! type ansible &> /dev/null ; then
+  sudo apt -y install ansible
+fi
 sudo apt -y remove --purge libreoffice* firefox
 sudo apt -y remove --purge thunderbird
 
